fb32867c6d62b60ff33b30956ee42d0f48f90b35
SergioJune <2217532592@qq.com>
2020-11-21 22:49:46 +0800

    349

---
+++ array/349.py
@@ -0,0 +1,32 @@
+"""
+给定两个数组，编写一个函数来计算它们的交集。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2]
+示例 2：
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[9,4]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        d = dict()
+        res = []
+        for i in nums1:
+            d[i] = i
+
+        for i in nums2:
+            if d.get(i, None) is not None:
+                res.append(i)
+                del d[i]
+        return res
