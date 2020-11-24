6ea3ace6db6bf99fdf878d200f78f4cebc410538
SergioJune <2217532592@qq.com>
2020-11-24 21:12:37 +0800

    42接雨水

---
+++ array/42.py
@@ -0,0 +1,48 @@
+"""
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+
+示例 1：
+输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+输出：6
+解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+
+
+示例 2：
+输入：height = [4,2,0,3,2,5]
+输出：9
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/trapping-rain-water
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def trap(self, height: list) -> int:
+        if len(height) < 2:
+            return 0
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        res = 0
+        while left < right:
+            print(height[left], height[right])
+            if height[left] < height[right]:
+                left += 1
+                if height[left] < left_max:
+                    res += left_max - height[left]
+                else:
+                    left_max = height[left]
+            else:
+                right -= 1
+                if height[right] < right_max:
+                    res += right_max - height[right]
+                else:
+                    right_max = height[right]
+
+        return res
+
+
+if __name__ == "__main__":
+    solu = Solution()
+    print(solu.trap([4,2,0,3,2,5]))
